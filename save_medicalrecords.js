ac21f800d4c6dc118857dae5bfbb768774de69c1
Smriti Srivastava <42644411+SmritiSrivastav@users.noreply.github.com>
2020-06-13 17:02:40 +0530

    Add files via upload

---
+++ save_medicalrecords.js
@@ -0,0 +1,14 @@
+var connection = require('./../config');
+var express=require('express');
+var router=express.Router();
+
+module.exports.register=function(req,res){
+	connection.query("insert into medical_records (patient_id,doctor_id,symptoms,medication_prescribed) values (?,?,?,?)",[req.body.patient_id,req.body.doctor_id,req.body.symptoms,req.body.prescribed],function(errormain,resultmain,fieldmain){
+		if(errormain){res.json({message:errormain});}
+		else{
+			res.json({
+			status:true,
+			message: 'Medical Record Saved.'});
+		}
+	});
+}
