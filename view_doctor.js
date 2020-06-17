3d74c18e6c456dfd85e0852c0c53e5f0027e6e26
Smriti Srivastava <42644411+SmritiSrivastav@users.noreply.github.com>
2020-06-17 22:40:40 +0530

    Update view_doctor.js

--- view_doctor.js
+++ view_doctor.js
@@ -3,7 +3,7 @@ var express=require('express');
 var router=express.Router();
 
 module.exports.view_doctor=function(req,res){
-	connection.query("select doctor_name,experience,specialization,department_name from doctor natural join department on department.department_id=doctor.department_id where department.department_id=?",[req.body.department_id],function(errormain,resultmain,fieldmain){
+	connection.query("select username,experience,specialization,department_name from ((doctor inner join department on department.department_id=doctor.department_id) inner join login on login.user_id=doctor.user_id_ref) where department.department_id=?",[req.body.department_id],function(errormain,resultmain,fieldmain){
 		if(errormain){res.json({message:errormain});}
 		else{
 			res.json({
