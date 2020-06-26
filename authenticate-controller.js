f078a23d6e7a52c69e5618d3645dc446e3353c46
Smriti Srivastava <42644411+SmritiSrivastav@users.noreply.github.com>
2020-06-27 00:12:58 +0530

    Add files via upload

--- authenticate-controller.js
+++ authenticate-controller.js
@@ -3,6 +3,24 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var app = express();
+
+function doctor(user_id){
+		connection.query('select doctor_id from doctor where user_id_ref=?',[user_id],function(errord,resultsd,fieldsd){
+					if(errord){
+						return;
+					/*res.json({
+				status:false,
+				title: errord,
+				message:"There is something wrong with the query"
+            });	*/
+					}
+			else{
+				return resultsd[0].doctor_id;
+			}
+				});
+				
+}
+
 module.exports.authenticate=function(req,res){
 	var choice=req.body.choice;
     var username=req.body.username;
@@ -18,36 +36,35 @@ module.exports.authenticate=function(req,res){
             })
       }else{
         if(results.length >0){
+			user_id=results[0].user_id;
 			if(results[0].user_type==1){
-				connection.query('select doctor_id from doctor where user_id_ref=?',[results[0].user_id],function(errord,resultsd,fieldsd){
-					if(errord){
-					res.json({
-				status:false,
-				title: errord,
-				message:"There is something wrong with the query"
-            });	
-					}
-			else{
-				user_id=resultsd[0].doctor_id;
-			}
-				});
+			user_id=doctor(user_id);
 				
-				
-			}
-			else{
-				user_id=results[0].user_id;
-			}
-			//Creation of sessions using cookie-parser
+			}	
 			req.session.user_id=user_id;
 			req.session.username=results[0].username;
+			//req.session.email=results[0].email;
+			//req.session.phone=results[0].phone;
 				
                 res.json({
                     status:true,
                     message:'User authenticated',
 
                 });
+            /*}else{
+                res.json({
+                  status:false,
+                  message:"Username and password does not match"
+                 });
+            }*/
          
         }
+       /* else{
+          res.json({
+              status:false, 			  
+            message:"username does not exits "
+          });
+        }*/
       }
     });
 }
