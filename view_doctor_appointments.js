df53c459044e72a8e5418d7df8c972dff710358b
Smriti Srivastava <42644411+SmritiSrivastav@users.noreply.github.com>
2020-07-02 00:03:06 +0530

    Add files via upload

--- view_doctor_appointments.js
+++ view_doctor_appointments.js
@@ -3,7 +3,8 @@ var express=require('express');
 var router=express.Router();
 //doctor_id from session
 module.exports.doctor_appointments=function(req,res){
-	connection.query("select time,firstname,lastname,phone,age,gender from appointments inner join login on appointments.patient_id=login.user_id where doctor_id=? and date=?",[req.body.doctor_id,req.body.date],function(errormain,resultmain,fieldmain){
+	if(req.session.user_id){
+	connection.query("select time,phone,age,gender,firstname,lastname from appointments inner join login on appointments.patient_id=login.user_id where doctor_id=? and date=? and active=1",[req.session.user_id,req.body.date],function(errormain,resultmain,fieldmain){
 		if(errormain){res.json({message: "Something went wrong!!"});}
 		else{
 			res.json({
@@ -11,4 +12,8 @@ module.exports.doctor_appointments=function(req,res){
 			message:resultmain});
 		}
 	});
+	}
+	else{
+		res.json({message: "Please login."});
+	}
 }
